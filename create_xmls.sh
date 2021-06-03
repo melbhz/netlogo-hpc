7ab822e3eeee8d6d1b8d5f4b49f44bcee66e1dac
Haifeng Zhao <36873693+melbhz@users.noreply.github.com>
2021-06-03 21:52:22 +1000

    Update create_xmls.sh

--- create_xmls.sh
+++ create_xmls.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-BEHAVIORSPACE_NAME='MainRun' #REVISE HERE (THIS ASSUME THE NAME IS UNIQUE)
-NETLOGO_MODEL='/path/to/your/nlogo/file/for/example/model.nlogo' #REVISE HERE
+BEHAVIORSPACE_NAME='MainRun' #REVISE HERE (This is the Experiment name - This assume this .nlogo file only have one experiment named for example MainRun)
+NETLOGO_MODEL='/path/to/your/nlogo/file/xxxx.nlogo' #REVISE HERE
 OUTPUT_FILE='Experiment.xml'
 
-#creating an experiment setting file - Experiment.xml for NetLogo running in headless mode
-#<?xml version="1.0" encoding="UTF-8"?>
-#<!DOCTYPE experiments SYSTEM "behaviorspace.dtd">
-echo '<?xml version="1.0" encoding="UTF-8"?>' > $OUTPUT_FILE #use "$OUTPUT_FILE" if this file name includes space; same for other file names
-echo '<!DOCTYPE experiments SYSTEM "behaviorspace.dtd">' >> $OUTPUT_FILE
+#1. creating an experiment setup-file - Experiment.xml for NetLogo running in headless mode
+echo '<?xml version="1.0" encoding="UTF-8"?>' > "$OUTPUT_FILE" #use "$OUTPUT_FILE" if this file name includes space; same for other file names
+echo '<!DOCTYPE experiments SYSTEM "behaviorspace.dtd">' >> "$OUTPUT_FILE"
 
-echo '<experiments>' >> $OUTPUT_FILE
-sed -n -e "/<experiment name=\"$BEHAVIORSPACE_NAME\"/,/<\/experiment>/p" "$NETLOGO_MODEL" >> $OUTPUT_FILE
-echo '</experiments>' >> $OUTPUT_FILE
+echo '<experiments>' >> "$OUTPUT_FILE"
+sed -n -e "/<experiment name=\"$BEHAVIORSPACE_NAME\"/,/<\/experiment>/p" "$NETLOGO_MODEL" >> "$OUTPUT_FILE"
+echo '</experiments>' >> "$OUTPUT_FILE"
 
 echo "$OUTPUT_FILE created!"
-#echo 'done!'
 
 
 
 
-#split Experiment.xml by a parameter - "rand_seed" in this example
+#2. split Experiment.xml by a parameter - "rand_seed" in this example
 XML_HEAD='head.xml'
 XML_SEED='seed.xml'
 XML_TAIL='tail.xml'
@@ -36,10 +33,7 @@ echo "$XML_HEAD, $XML_SEED, $XML_TAIL created!"
 
 
 
-#combine files to create exp_[i].xml files where each exp_[i].xml have only one rand_seed value 
-#XML_HEAD='head.xml'
-#XML_SEED='seed.xml'
-#XML_TAIL='tail.xml'
+#3. combine files to create exp_[i].xml files where each exp_[i].xml have only one rand_seed value 
 OUTPUT_DIR='xmls'
 PREFIX='exp_'
 FILE_EXTENTION='.xml'
@@ -64,5 +58,5 @@ echo 'in the xmls folder created!'
 
 
 
-#clean auxilary files (optional)
-#rm $OUTPUT_FILE $XML_HEAD $XML_SEED $XML_TAIL $tmp_seedtail_file
+#4. clean temporary files (optional), remove the # in the next line if you want to clear temporary files
+#rm "$OUTPUT_FILE" "$XML_HEAD" "$XML_SEED" "$XML_TAIL" "$tmp_seedtail_file"
