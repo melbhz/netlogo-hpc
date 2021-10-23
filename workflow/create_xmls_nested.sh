35f6bf3615497429a4823a41fe609dbcdd448a3c
Haifeng Zhao <haifeng.zhao@unimelb.edu.au>
2021-10-23 15:05:09 +1100

    Update create_xmls_nested.sh

--- workflow/create_xmls_nested.sh
+++ workflow/create_xmls_nested.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BEHAVIORSPACE_NAME='HPC_Experiment' #REVISE HERE
 NETLOGO_MODEL='/data/gpfs/projects/punim1439/workflow/netlogo_hpc/Wolf_Sheep_Predation/Wolf Sheep Predation HPC.nlogo' #REVISE HERE
-declare -a SPLIT_BY_VARIABLES=("wolf-gain-from-food" "wolf-reproduce") #REVISE HERE
+declare -a SPLIT_BY_VARIABLES=("wolf-gain-from-food" "wolf-reproduce") #REVISE HERE: what variables to use to split the experiment
 
 
 #1. creating an experiment setup-file - Experiment.xml for NetLogo running in headless mode
@@ -26,6 +26,7 @@ OUTPUT_FILE='Experiment.xml'
 CreateExperimentXML "$BEHAVIORSPACE_NAME" "$NETLOGO_MODEL" "$OUTPUT_FILE"
 OUTPUT_DIR='xmls'
 mkdir -p $OUTPUT_DIR
+echo "move $OUTPUT_FILE to $OUTPUT_DIR"
 mv "$OUTPUT_FILE" "$OUTPUT_DIR"
 
 #2. split Experiment.xml by a variable
@@ -44,18 +45,15 @@ function CreateXMLbyParameter(){
 	#sed '/<?xml version="1.0" encoding="UTF-8"?>/,/<enumeratedValueSet variable="repetitions">/d' "$INPUT_FILE" > $tmp_seedtail_file
 	sed -n -e '/<?xml version="1.0" encoding="UTF-8"?>/,/<enumeratedValueSet variable="'"$split_by_var"'">/p' "$INPUT_FILE" > $XML_HEAD
 	sed '/<?xml version="1.0" encoding="UTF-8"?>/,/<enumeratedValueSet variable="'"$split_by_var"'">/d' "$INPUT_FILE" > $tmp_seedtail_file
-
 	sed -n -e '/<\/enumeratedValueSet>/,/<\/experiments>/p' "$tmp_seedtail_file" > $XML_TAIL
 	sed '/<\/enumeratedValueSet>/,/<\/experiments>/d' "$tmp_seedtail_file" > $XML_SEED
 
-	echo "$XML_HEAD, $XML_SEED, $XML_TAIL created!"
+	#echo "$XML_HEAD, $XML_SEED, $XML_TAIL created!"
 
 	#3. combine files to create exp_[i].xml files where each exp_[i].xml have only one rand_seed value 
-	#OUTPUT_DIR='xmls'
-	
+	#OUTPUT_DIR='xmls'	
 	local PREFIX='exp_'
 	local FILE_EXTENTION='.xml'
-
 	#mkdir -p $OUTPUT_DIR
 	#n=1	
 
@@ -70,7 +68,7 @@ function CreateXMLbyParameter(){
 	echo $output
 	n=$((n+1))
 	done < $XML_SEED
-	echo 'in the xmls folder created!'
+	echo 'in xmls folder created!'
 	
 	#4. clean temporary files (optional)
 	rm "$XML_HEAD" "$XML_SEED" "$XML_TAIL" "$tmp_seedtail_file"
@@ -80,16 +78,22 @@ function CreateXMLbyParameter(){
 
 ######
 TMP_INPUT_DIR='xmls_input'
+#loop over all split by variables
 for var in "${SPLIT_BY_VARIABLES[@]}"
 do
-   echo "$var"
+   echo "splitting experiment by \"$var\" ..."
    nextn=1
+   echo "feed output as input to function"
    mv -v "$OUTPUT_DIR" "$TMP_INPUT_DIR"
    mkdir $OUTPUT_DIR
-   for filename in $TMP_INPUT_DIR/*.xml; do   
-		echo "$filename"
-		CreateXMLbyParameter "$filename" "$OUTPUT_DIR" "$var" $nextn
+   for input_file in $TMP_INPUT_DIR/*.xml; do   
+		echo "spliting $input_file"
+		CreateXMLbyParameter "$input_file" "$OUTPUT_DIR" "$var" $nextn
 		nextn=$?
    done
    rm -rf "$TMP_INPUT_DIR"
 done
+
+total_xmlfiles_created=$((nextn-1))
+echo "In total, $total_xmlfiles_created xml files were created in xmls folder."
+echo "Remeber to revise your slurm file to use $total_xmlfiles_created in jobarray, i.e., #SBATCH --array=1-$total_xmlfiles_created"
